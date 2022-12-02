963eaff374721695344ccb4d7d325e0164375c83
Stephen Fabian <fabianstephen@gmail.com>
2022-12-02 10:46:34 -0700

    adds notes.rb file with query example

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
-  # *** for testing in graphiQL ***
+  # *** for testing queries in graphiQL ***
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-
-  # *** for production/postman *** 
-  # get "/graphql", to: "graphql#execute"
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
