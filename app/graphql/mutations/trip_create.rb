7322969a41d3add8f1ee3c45a9cc2506e37bca3b
Stephen Fabian <fabianstephen@gmail.com>
2022-12-04 19:49:53 -0700

    adds images to image_selector method in trip_create mutation

--- app/graphql/mutations/trip_create.rb
+++ app/graphql/mutations/trip_create.rb
@@ -17,7 +17,10 @@ module Mutations
 
     def image_selector(category)
       "https://www.sandals.com/blog/content/images/2020/03/Sandals-Barbados-Beach-Palm-Trees-Beach.jpg" if category == 2
+      "https://s.yimg.com/ny/api/res/1.2/kVLRa0dKvjn2jE11kUOJxg--/YXBwaWQ9aGlnaGxhbmRlcjt[…]/media.zenfs.com/en/galvanized/03890eb402d901af3fc0d8d7a3e64b35" if category == 3
       "https://www.lonestar.edu/images/internationalTravel.jpg" if category == 4
+      "https://hips.hearstapps.com/clv.h-cdn.co/assets/16/49/gettyimages-492392636-1.jpg" if category == 5
+      "https://10-raisons.fr/wp-content/uploads/2021/03/roadtrip-une-aventure-fabuleuse-et-inoubliable.jpg" if category == 6
     end
   end
 end
