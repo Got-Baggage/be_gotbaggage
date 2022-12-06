d22bd185eb5c2a86fb35d0995c1aecb6bfa84c4c
Nikky Rojas <rojas7313@gmail.com>
2022-12-06 11:49:33 -0700

    model dependent destroy

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-  has_many :items, through: :trip_items
+  has_many :items, through: :trip_items, dependent: :destroy
   validates_presence_of :name, :category
   enum category:[:essentials, :beach, :camping, :international, :city, :snowsports, :roadtrip]
 end
