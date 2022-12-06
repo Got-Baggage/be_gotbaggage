296631be7b05447607ba53eb3971d5f21bdaba9c
Stephen Fabian <fabianstephen@gmail.com>
2022-12-05 23:54:51 -0700

    adds tests for query_type.rb

---
+++ spec/queries/items/category_names_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do
+  describe 'category_names' do
+    it 'returns all unique Trip categories' do
+      result = BeGotbaggageSchema.execute(query).as_json
+
+      expect(result["data"]["categoryNames"]).to be_an(Array)
+      result["data"]["categoryNames"].each do |category|
+        expect(category).to be_a(String)
+      end
+    end
+  end
+  
+  def query 
+    <<~GQL
+      query {
+        categoryNames
+      }
+    GQL
+  end
+end
