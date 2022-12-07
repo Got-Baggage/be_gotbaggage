21de40d056382beb01365c2c24aef8e2cfe290a6
Stephen Fabian <fabianstephen@gmail.com>
2022-12-07 11:28:41 -0700

    corrects has_many model associations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ApplicationRecord
+  has_many :trip_items, dependent: :destroy
   has_many :items, through: :trip_items, dependent: :destroy
   validates_presence_of :name, :category
   enum category:[:essentials, :beach, :camping, :international, :city, :snowsports, :roadtrip]
