16857c8ecd86966c5d75c60de7b106ebcf2ce3f7
Nikky Rojas <rojas7313@gmail.com>
2022-12-08 15:03:31 -0700

    fix itemCreate test

--- spec/graphql/mutations/items/item_create_spec.rb
+++ spec/graphql/mutations/items/item_create_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'ItemCreate', type: :request do
   describe '.resolve' do
     before :each do
-      # @trip = Trip.create!(id: 1, name: 'Disney Trip', category: 'city', traveler: 'Mickey',
-      #                      image: 'https://imageofthecity.com')
+      @trip = Trip.create!(id: 1, name: 'Disney Trip', category: 'city', traveler: 'Mickey',
+                            image: 'https://imageofthecity.com')
       @query = <<~GQL
         mutation{
-          itemCreate(input: {tripId: 1, itemName: “boss item”})
+          itemCreate(input: {tripId: 1, itemName: "boss item", category: 1})
           {
             item{
               name
@@ -19,11 +19,11 @@ RSpec.describe 'ItemCreate', type: :request do
       @result = BeGotbaggageSchema.execute(@query).as_json
     end
 
-    xit 'creates an Item' do
+    it 'creates an Item' do
       expect(@result['data']).to be_a(Hash)
       expect(@result['data']['itemCreate']).to be_a(Hash)
       expect(@result['data']['itemCreate']['item']).to be_a(Hash)
-      expect(@result['data']['itemCreate']['user']['itemName']).to eq('boss item')
+      expect(@result['data']['itemCreate']['item']['name']).to eq('boss item')
     end
   end
 end
--- spec/graphql/mutations/items/item_update_spec.rb
+++ spec/graphql/mutations/items/item_update_spec.rb
@@ -3,20 +3,25 @@ require 'rails_helper'
 RSpec.describe 'ItemUpdate', type: :request do
   describe '.resolve' do
     before :each do
+      @trip = Trip.create!(id: 2, name: 'Disney Trip', category: 'city', traveler: 'Mickey', image: 'whatever.com')
+      @item = Item.create!(id: 234, name: 'moouthwash', category: 'city')
+      @trip_item = TripItem.create!(trip_id: 2, item_id: 234)
       @query = <<~GQL
         mutation{
-          itemUpdate(input: {id: 1, itemName: “bossiest item”})
-          {
-            item{
-              name
-            }
+          itemUpdate(input: {id: 234, itemName: "mouthwash item"})
+          # {
+          #   item{
+          #     name
+          #     id
+          #   }
           }
         }
       GQL
     end
 
-    xit 'updates an Item' do
+    it 'updates an Item' do
       post '/graphql', params: {query: @query}
+      # binding.pry
       expect(Trip.count).to eq(1)
 
       json = JSON.parse(response.body)
