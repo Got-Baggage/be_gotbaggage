c1c49580ba8b5cd141c7cb6e5d10a6f21e936ef6
Nicole Esqier <nicole.esquer5@gmail.com>
2022-12-08 04:40:29 -0700

    removed location from validations as it will not be used

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   has_many :trip_items, dependent: :destroy
   has_many :items, through: :trip_items, dependent: :destroy
-  validates_presence_of :name, :traveler, :category, :image, :location
+  validates_presence_of :name, :category, :traveler, :image
   enum category:[:essentials, :beach, :camping, :international, :city, :snowsports, :roadtrip]
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Trip, type: :model do
     it { should validate_presence_of(:traveler) }
     it { should validate_presence_of(:category) }
     it { should validate_presence_of(:image) }
-    it { should validate_presence_of(:location) }
   end
 
   describe 'relationships' do
